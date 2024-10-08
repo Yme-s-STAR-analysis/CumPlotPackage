08ba717b4429c167c67f4eb8e3db38a0a84dd2e0
Yme <yghuang@mails.ccnu.edu.cn>
2024-10-08 03:03:42 +0200

    v2.3: New demo

--- PlotCent.cxx
+++ PlotCent.cxx
@@ -12,28 +12,28 @@ void PlotCent() {
         "/Path-to-UrQMD/y0p5.root",
         "/Path-to-BES1/stat/y0p5.root",
         "/Path-to-BES1/sys/y0p5.root",
-        "/Path-to-BES2/stat/RefMult3_y0p5.root",
-        "/Path-to-BES2/sys/RefMult3_y0p5.root",
-        "/Path-to-BES2/stat/RefMult3X_y0p5.root",
-        "/Path-to-BES2/sys/RefMult3X_y0p5.root"
+        "/Path-to-BES2/stat/y0p5.root",
+        "/Path-to-BES2/sys/y0p5.root"
+    };
+
+    bool cbwca[nf] = {
+        false, false, false, false, false
     };
 
     int mcolor[nf] = {
         kGreyYellow,
         kBlack, kBlack,
         kRed, kRed,
-        kOrange, kOrange
     };
     
     int mstyle[nf] = {
-        21, 30, 30, 20, 20, 21, 21
+        21, 30, 30, 20, 20
     };
     double msize[nf] = {
-        1.3, 2.2, 2.2, 1.3, 1.3, 1.3, 1.3
+        1.3, 2.2, 2.2, 1.3, 1.3
     };
     const char* pstyle[nf] = {
         "e3"
-        "epl", "e[]", 
         "epl", "e[]", 
         "epl", "e[]"
     };
@@ -48,21 +48,14 @@ void PlotCent() {
     lat->SetTextFont(64);
     lat->SetTextSize(12);
 
-    // now Npart is determined when do the calculation (CBWC)
-    // double Npart[9] = {
-    //     339, 289, 224, 158, 109, 71, 44, 25, 13
-    // };
-
     sMode_t sMode = 0; 
-    for (cMode_t cMode=0; cMode<1; cMode++) {
+    for (cMode_t cMode=0; cMode<2; cMode++) {
         for (pMode_t pMode=0; pMode<3; pMode++) {
             if (cMode == 1 && pMode == 0) { continue; }
-            pm->Init(nf, fNames, mcolor, mstyle, msize, pstyle, pMode, cMode, sMode);
+            pm->Init(nf, fNames, cbwca, mcolor, mstyle, msize, pstyle, pMode, cMode, sMode);
             pm->SetDefaultLabelLocation();
             pm->ReadGraph();
-            // for (int i=0; i<nf; i++) { // if some of the files are using other Npart, should not use this loop. Set them separately instead!
-            //     pm->SetGraphXArray(9, i, Npart);
-            // }
+            // pm->SetGraphXArray(9, i, Npart); 
             pm->SetNp(9);
             pm->SetUrQMD(0);
             // pm->SetLabelLocationX(5, 0.675); 
@@ -72,9 +65,10 @@ void PlotCent() {
             pm->SetLegend(2, 3, "BES-II Corr. (RefMult3)", "ep");
             pm->SetLegend(3, 5, "BES-II Corr. (RefMult3X)", "ep");
             pm->Plot();
-            pm->AddLegend("14.6", 0.5);
+            pm->AddLegend("ENERGY", 0.5);
 
-            lat->DrawLatexNDC(0.75, 0.04, "[]: Systematic uncertainty");
+            // lat->DrawLatexNDC(0.75, 0.04, "[]: Systematic uncertainty");
+            // lat->DrawLatexNDC(0.75, 0.35, "0 - 5%");
             pm->Save(sname);
         }
     }
